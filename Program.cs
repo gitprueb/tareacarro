58910e3020f3cf579a6cd99e3985b5b62e6dab37
primera <eflorianc@miumg.edu.gt>
2022-03-27 16:08:48 -0600

    carro

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using tarea_carro.clases;
+
+carro erCarro = new carro("bmw", 2022, "azul");
+
+Console.WriteLine($"EL carro de Ericson es {erCarro.Marca}");
+Console.WriteLine($"el carro de Ericson es modelo{erCarro.Modelo}");
+
+
+
+erCarro.EncenderMotor();
+Console.WriteLine(erCarro.Bocina());
+
+
+
+for (int i = 0; i < 21; i++)
+{
+    erCarro.Acelerar();
+    
+
+}
+for (int i = 0; i < 10; i++)
+{
+    erCarro.Desacelerar();
+    
+}
+for (int i = 0; i < 4; i++)
+{
+    erCarro.FRENAR();
+    
+}
+
+
+Console.WriteLine(erCarro.Bocina());
+erCarro.ApagarMotor();
---
+++ clases/carro.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_carro.clases
+{
+    internal class carro
+
+        {
+        public string Marca { get; set; }
+        public int Modelo { get; set; }
+        public String Color { get; set; }
+
+        private int Encendido = 0;
+        private int apagado = 0; 
+        private int va = 0;
+        private const int MV = 200;
+
+        public carro(string nombre, int modelo, string color)
+        {
+            Marca = nombre;
+            Modelo = modelo;
+            Color = color;
+            Encendido = 0;
+            this.va = 0;
+
+
+        }
+        public string vla()
+        {
+            return $"la velocidad es  {va} KPH";
+        }
+
+        public void EncenderMotor()
+        {
+            if (Encendido == 0)
+            {
+                Console.WriteLine("mummmmmm carro encendido");
+                Encendido = 1;
+            }
+            else
+            {
+                Console.WriteLine("el carro ya estaba encendido");
+            }
+        }
+        public string Acelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"el carro esta apagado";
+                return mensaje;
+            }
+
+
+
+            va += 10;
+           
+            if (va <= MV)
+            {
+                mensaje = $" la velocidad es {va} KPH";
+            }
+            else
+            {
+                va = MV;
+                mensaje = $"{va} KPH vas sobre el limite de velocidad desacelera";
+
+                Bocina();
+            }
+
+
+            Console.WriteLine(mensaje);
+            return mensaje;
+
+
+
+        }
+        public string Desacelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"el carro esta apagado";
+                return mensaje;
+            }
+
+
+
+            va -= 10;
+            if (va <= MV)
+            {
+                mensaje = $"desacelerando {va} KPH";
+            }
+            
+
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+        public string FRENAR()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"al carro esta apagado";
+                return mensaje;
+            }
+
+
+
+            va -= 25;
+            if ((va <= MV) && (va > 0))
+            {
+                mensaje = $"Vas frenando {va} KPH";
+            }
+            else
+            {
+                mensaje = "ya freno";
+            }
+
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+
+        public void ApagarMotor()
+        {
+            if (apagado == 0)
+            {
+                Console.WriteLine("carro apagado");
+                apagado = 1;
+            }
+            else
+            {
+                Console.WriteLine("ya estaba apagado");
+            }
+        }
+        public string Bocina()
+        {
+            string mensaje = "";
+           
+            {
+                Console.Beep();
+                Console.WriteLine("beep");
+            }
+           
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+
+        }
+}
